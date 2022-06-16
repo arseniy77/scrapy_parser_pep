e6140ad3d885e2af1b7b1a8d123085e344c68d22
Arseniy Gaintsev <arseniy.gaintsev@gmail.com>
2022-06-16 22:15:18 +0300

    csv_done

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -8,19 +8,28 @@ class PepSpider(scrapy.Spider):
 
     def parse(self, response):
         pep_table = response.css('section#numerical-index')
-        rows = pep_table.css('a[href^="pep-"]')
-        for row in rows[1:7]:
+        # rows = pep_table.css('a[href^="pep-"]')
+        rows = pep_table.css('tr')
+        for row in rows[1::]:
+            pep_number, pep_name = row.css('a::text').getall()
+            link = row.css('a.pep.reference.internal::attr(href)').get()
+            # print('LINK', link)
+            # print(pep_number, pep_name)
 
+            cb_kwargs = {'pep_number': pep_number,
+                         'pep_name': pep_name,
+                         }
+            # print(row.css('a.pep.reference.internal::text').get())
+            yield response.follow(
+                link, callback=self.parse_pep, cb_kwargs=cb_kwargs
+            )
 
-            cb_kwargs = {}
-            print(row.css('a.pep.reference.internal::text').get())
-            yield response.follow(row, callback=self.parse_pep)
-
-    def parse_pep(self, response):
-        number = response.css('.breadcrumbs li::text').getall()[2].strip('PEP ')
-        name = response.css('h1.page-title::text').get()[len(number)+7::]
+    def parse_pep(self, response, pep_number, pep_name):
+        # number = response.css('.breadcrumbs li::text').getall()[2].strip('PEP ')
+        # name = response.css('h1.page-title::text').get()[len(number)+7::]
+        print(pep_name)
         yield {
-            'number': number,
-            'name': name,
+            'number': pep_number,
+            'name': pep_name,
             'status': response.css('dd.field-even::text').get(),
         }
